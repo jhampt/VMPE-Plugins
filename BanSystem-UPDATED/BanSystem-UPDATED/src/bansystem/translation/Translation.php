61c22754407e24c046e539aea8583d955ef84dac
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-03-14 22:48:04 +0000

    Update Translation.php

--- BanSystem-UPDATED/BanSystem-UPDATED/src/bansystem/translation/Translation.php
+++ BanSystem-UPDATED/BanSystem-UPDATED/src/bansystem/translation/Translation.php
@@ -14,31 +14,31 @@ class Translation {
             case "noPermission":
                 return TextFormat::RED . "Not showing command information due to self-leak issues.";
             case "playerNotFound":
-                return TextFormat::GOLD . "Player is not online.";
+                return TextFormat::GOLD . "§2Player is not online.";
             case "playerAlreadyBanned":
-                return TextFormat::GOLD . "Player is already banned.";
+                return TextFormat::GOLD . "§2Player is already banned.";
             case "ipAlreadyBanned":
-                return TextFormat::GOLD . "Player is already IP banned.";
+                return TextFormat::GOLD . "§2Player is already IP banned.";
             case "ipNotBanned":
-                return TextFormat::GOLD . "IP address is not banned.";
+                return TextFormat::GOLD . "§2IP address is not banned.";
             case "ipAlreadyMuted":
-                return TextFormat::GOLD . "IP address is already muted.";
+                return TextFormat::GOLD . "§2IP address is already muted.";
             case "playerNotBanned":
-                return TextFormat::GOLD . "Player is not banned.";
+                return TextFormat::GOLD . "§2Player is not banned.";
             case "playerAlreadyMuted":
-                return TextFormat::GOLD . "Player is already muted.";
+                return TextFormat::GOLD . "§2Player is already muted.";
             case "playerNotMuted":
-                return TextFormat::GOLD . "Player is not muted.";
+                return TextFormat::GOLD . "§2Player is not muted.";
             case "ipNotMuted":
-                return TextFormat::GOLD . "IP address is not muted.";
+                return TextFormat::GOLD . "§2IP address is not muted.";
             case "playerAlreadyBlocked":
-                return TextFormat::GOLD . "Player is already blocked.";
+                return TextFormat::GOLD . "§2Player is already blocked.";
             case "playerNotBlocked":
-                return TextFormat::GOLD . "Player is not blocked.";
+                return TextFormat::GOLD . "§2Player is not blocked.";
             case "ipAlreadyBlocked":
-                return TextFormat::GOLD . "IP address is already blocked.";
+                return TextFormat::GOLD . "§2IP address is already blocked.";
             case "ipNotBlocked":
-                return TextFormat::GOLD . "IP address is not blocked.";
+                return TextFormat::GOLD . "§2IP address is not blocked.";
             default:
                 throw new TranslationFailedException("Failed to translate.");
         }
@@ -52,7 +52,7 @@ class Translation {
             case "usage":
                 $command = $parameters[0];
                 if ($command instanceof Command) {
-                    return TextFormat::DARK_GREEN . "Usage: " . TextFormat::GREEN . $command->getUsage();
+                    return TextFormat::DARK_GREEN . "§aPlease use: " . TextFormat::AQUA . $command->getUsage();
                 } else {
                     throw new InvalidArgumentException("Parameter index 0 must be the type of Command.");
                 }
